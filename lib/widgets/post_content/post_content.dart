08e3b5eaa1b3c5cf8a1073db3b1557273e8b1106
yshean <yshean@gmail.com>
2021-01-18 16:23:15 +0800

    feat: Implement post details - ImageContent

--- lib/widgets/post_content/post_content.dart
+++ lib/widgets/post_content/post_content.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:reddit_client/models/post.dart';
 import 'package:reddit_client/utils/get_post_type.dart';
-import 'package:reddit_client/widgets/post_content/gif_video_content.dart';
-import 'package:reddit_client/widgets/post_content/self_post_content.dart';
-import 'package:reddit_client/widgets/post_content/video_content.dart';
+
+import 'gif_video_content.dart';
+import 'image_content.dart';
+import 'self_post_content.dart';
+import 'video_content.dart';
 
 class PostContent extends StatelessWidget {
   final Post post;
@@ -14,6 +16,8 @@ class PostContent extends StatelessWidget {
   Widget build(BuildContext context) {
     if (post.postType == PostType.SelfPost)
       return SelfPostContent(text: post.selfText);
+    if (post.postType == PostType.Image)
+      return ImageContent(url: post.url.toString());
     if (post.postType == PostType.Video) return VideoContent(post: post);
     if (post.postType == PostType.GifVideo) return GifVideoContent(post: post);
     return Text('Unimplemented PostType: ${post.postType}');
